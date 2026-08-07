9576b4f98df7ecd6f21fe01b4dc47b0cfd7889f1
vong321654 <141673546+vong321654@users.noreply.github.com>
2026-08-07 13:50:30 +0700

    test:deploy

--- app/manageUser/page.tsx
+++ app/manageUser/page.tsx
@@ -4,7 +4,7 @@ export default function ManageUserPage() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="p-8 bg-white rounded shadow-md">
-        <h1 className="text-2xl font-bold mb-4">จัดการผู้ใช้งาน</h1>
+        <h1 className="text-2xl font-bold mb-4">จัดการผู้ใช้งาน...</h1>
         <p>หน้านี้กำลังอยู่ระหว่างการพัฒนา...</p>
       </div>
     </div>
