cf094d0175568cdd5ce9af9bc724350a370e6b96
Wannachol Pinprom <nat@MacBook-Air-khxng-Wannachol.local>
2026-09-17 01:15:00 +0700

    fix: Sidebar display

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { usePathname } from "next/navigation";
 import { AdminSessionProvider } from "./AdminSessionContext";
 import AdminSideBar from "./components/AdminSideBar";
 
@@ -6,6 +9,7 @@ export default function AdminLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
   const menuItems = [
     { name: "สินค้า", link: "/admin/product" },
     { name: "คลังสินค้า", link: "/admin/inventory" },
@@ -13,9 +17,10 @@ export default function AdminLayout({
     { name: "หนี้ค้าง", link: "/admin/debts" },
     { name: "ผู้ใช้", link: "/admin/users" },
   ];
+
   return (
     <div className="" style={{ display: "flex", minHeight: "100vh" }}>
-      <AdminSideBar HeaderName="Admin Panel" menuItems={menuItems} />
+      {pathname !== "/admin/login" && <AdminSideBar HeaderName="Admin Panel" menuItems={menuItems} />}
       <AdminSessionProvider>{children}</AdminSessionProvider>
     </div>
   );
